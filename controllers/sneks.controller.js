969d4c33b3542df520783f06dbbf706d256a5f96
Noelia Roman <Noelia.Roman99@hotmail.com>
2022-03-22 18:07:55 -0700

    :Dcjhscjs

--- controllers/sneks.controller.js
+++ controllers/sneks.controller.js
@@ -7,4 +7,10 @@ sneksController.getSneks = async (req, res) => {
 
 };
 
+sneksController.addSnek = async (req, res) => {
+    const snek = new sneksModel(req.body);
+    await snek.save();
+    res.json({'status' : 'Serpiente almacenada exitosamente'});
+
+};
 module.exports = sneksController;
--- routes/sneks.routes.js
+++ routes/sneks.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const sneksController = require('../controllers/sneks.controller');
 
 router.get('/', sneksController.getSneks);
+router.post('/', sneksController.addSnek );
 
 module.exports = router;
